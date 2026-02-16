14d4e2d1b5d30631e3e41673c4caf86481d9d97a
Devendra <devendrachapagain100@gmail.com>
2026-02-17 01:00:10 +0545

    Hero Section Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,12 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
-      
     </html>
   );
 }
